717a8c3c1b3275fcca3d65965b07ab0dd60b3f3c
Matt Garrett <mattga@gmail.com>
2011-08-17 10:08:39 -0500

    Not using the window object, closes #3.

--- params.coffee
+++ params.coffee
@@ -1,60 +1,61 @@
 # a small library to create a URL by adding and removing params while
 # preserving existing params
-window.Params = (_location = location)->
+((root)->
+  root.Params = (_location = location)->
 
-  _search   = _location.search
-  _prefix   = _location.protocol + '//' + _location.host + _location.pathname
-  _params   = {}
+    _search   = _location.search
+    _prefix   = _location.protocol + '//' + _location.host + _location.pathname
+    _params   = {}
 
-  # build _params keys
-  if _search.indexOf('?') >= 0
-    # remove the leading ? character, and split on &
-    _search = _search[1..._search.length].split('&')
-    for pair in _search
-      pair = pair.split('=')
-      _params[pair[0]] = pair[1]
+    # build _params keys
+    if _search.indexOf('?') >= 0
+      # remove the leading ? character, and split on &
+      _search = _search[1..._search.length].split('&')
+      for pair in _search
+        pair = pair.split('=')
+        _params[pair[0]] = pair[1]
 
-  # build the full url, similar to location.href
-  href = ->
-    _prefix + search()
+    # build the full url, similar to location.href
+    href = ->
+      _prefix + search()
 
-  # build only the params, similar to location.search
-  search = ->
-    pairs = for key of _params
-      "#{key}=#{_params[key]}"
-    if pairs.length > 0
-      "?" + pairs.join('&')
-    else
-      ""
+    # build only the params, similar to location.search
+    search = ->
+      pairs = for key of _params
+        "#{key}=#{_params[key]}"
+      if pairs.length > 0
+        "?" + pairs.join('&')
+      else
+        ""
 
-  # simple setting of a pair
-  _set = (key, value)->
-    _params[key] = value
+    # simple setting of a pair
+    _set = (key, value)->
+      _params[key] = value
 
-  set = ->
-    arg = arguments[0]
-    # if a simple pair
-    if typeof arg is 'string' or typeof arg is 'number'
-      _set String(arg), arguments[1]
+    set = ->
+      arg = arguments[0]
+      # if a simple pair
+      if typeof arg is 'string' or typeof arg is 'number'
+        _set String(arg), arguments[1]
 
-    else if typeof arg is 'object'
-      for key of arg
-        _set key, arg[key]
-      arg
-    else
-      throw "Unexpected data type for: #{arg}. Should be a string, number, or object"
+      else if typeof arg is 'object'
+        for key of arg
+          _set key, arg[key]
+        arg
+      else
+        throw "Unexpected data type for: #{arg}. Should be a string, number, or object"
 
-  unset = (key)->
-    value = _params[key]
-    delete _params[key]
-    value
+    unset = (key)->
+      value = _params[key]
+      delete _params[key]
+      value
 
-  # export
-  {
-    href
-    search
-    get : (key)-> _params[key]
-    set
-    unset
-  }
-
+    # export
+    {
+      href
+      search
+      get : (key)-> _params[key]
+      set
+      unset
+    }
+)(this)
--- params.js
+++ params.js
@@ -1,70 +1,72 @@
 (function() {
-  window.Params = function(_location) {
-    var href, pair, search, set, unset, _i, _len, _params, _prefix, _search, _set;
-    if (_location == null) {
-      _location = location;
-    }
-    _search = _location.search;
-    _prefix = _location.protocol + '//' + _location.host + _location.pathname;
-    _params = {};
-    if (_search.indexOf('?') >= 0) {
-      _search = _search.slice(1, _search.length).split('&');
-      for (_i = 0, _len = _search.length; _i < _len; _i++) {
-        pair = _search[_i];
-        pair = pair.split('=');
-        _params[pair[0]] = pair[1];
+  (function(root) {
+    return root.Params = function(_location) {
+      var href, pair, search, set, unset, _i, _len, _params, _prefix, _search, _set;
+      if (_location == null) {
+        _location = location;
       }
-    }
-    href = function() {
-      return _prefix + search();
-    };
-    search = function() {
-      var key, pairs;
-      pairs = (function() {
-        var _results;
-        _results = [];
-        for (key in _params) {
-          _results.push("" + key + "=" + _params[key]);
+      _search = _location.search;
+      _prefix = _location.protocol + '//' + _location.host + _location.pathname;
+      _params = {};
+      if (_search.indexOf('?') >= 0) {
+        _search = _search.slice(1, _search.length).split('&');
+        for (_i = 0, _len = _search.length; _i < _len; _i++) {
+          pair = _search[_i];
+          pair = pair.split('=');
+          _params[pair[0]] = pair[1];
         }
-        return _results;
-      })();
-      if (pairs.length > 0) {
-        return "?" + pairs.join('&');
-      } else {
-        return "";
       }
-    };
-    _set = function(key, value) {
-      return _params[key] = value;
-    };
-    set = function() {
-      var arg, key;
-      arg = arguments[0];
-      if (typeof arg === 'string' || typeof arg === 'number') {
-        return _set(String(arg), arguments[1]);
-      } else if (typeof arg === 'object') {
-        for (key in arg) {
-          _set(key, arg[key]);
+      href = function() {
+        return _prefix + search();
+      };
+      search = function() {
+        var key, pairs;
+        pairs = (function() {
+          var _results;
+          _results = [];
+          for (key in _params) {
+            _results.push("" + key + "=" + _params[key]);
+          }
+          return _results;
+        })();
+        if (pairs.length > 0) {
+          return "?" + pairs.join('&');
+        } else {
+          return "";
         }
-        return arg;
-      } else {
-        throw "Unexpected data type for: " + arg + ". Should be a string, number, or object";
-      }
+      };
+      _set = function(key, value) {
+        return _params[key] = value;
+      };
+      set = function() {
+        var arg, key;
+        arg = arguments[0];
+        if (typeof arg === 'string' || typeof arg === 'number') {
+          return _set(String(arg), arguments[1]);
+        } else if (typeof arg === 'object') {
+          for (key in arg) {
+            _set(key, arg[key]);
+          }
+          return arg;
+        } else {
+          throw "Unexpected data type for: " + arg + ". Should be a string, number, or object";
+        }
+      };
+      unset = function(key) {
+        var value;
+        value = _params[key];
+        delete _params[key];
+        return value;
+      };
+      return {
+        href: href,
+        search: search,
+        get: function(key) {
+          return _params[key];
+        },
+        set: set,
+        unset: unset
+      };
     };
-    unset = function(key) {
-      var value;
-      value = _params[key];
-      delete _params[key];
-      return value;
-    };
-    return {
-      href: href,
-      search: search,
-      get: function(key) {
-        return _params[key];
-      },
-      set: set,
-      unset: unset
-    };
-  };
+  })(this);
 }).call(this);
